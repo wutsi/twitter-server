86cfcff204cb7f32d9d30695716ada14fe0da1fe
htchepannou <htchepannou@expedia.com>
2021-04-12 16:22:05 -0400

    use dynamic-tracking-context

--- src/main/kotlin/com/wutsi/twitter/config/TracingConfiguration.kt
+++ src/main/kotlin/com/wutsi/twitter/config/TracingConfiguration.kt
@@ -1,32 +1,26 @@
 package com.wutsi.twitter.config
 
-import com.wutsi.tracing.RequestTracingContext
-import com.wutsi.tracing.TracingContextProvider
+import com.wutsi.tracing.DynamicTracingContext
+import com.wutsi.tracing.TracingContext
 import feign.RequestInterceptor
-import org.springframework.beans.factory.`annotation`.Autowired
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
-import org.springframework.context.`annotation`.Bean
-import org.springframework.context.`annotation`.Configuration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import javax.servlet.Filter
-import javax.servlet.http.HttpServletRequest
 
 @Configuration
 public class TracingConfiguration(
     @Autowired
-    private val request: HttpServletRequest,
-    @Autowired
     private val context: ApplicationContext
 ) {
     @Bean
-    public fun tracingFilter(): Filter = com.wutsi.tracing.TracingFilter(tracingContextProvider())
+    public fun tracingFilter(): Filter = com.wutsi.tracing.TracingFilter(tracingContext())
 
     @Bean
-    public fun requestTracingContext(): RequestTracingContext = RequestTracingContext(request)
-
-    @Bean
-    public fun tracingContextProvider(): TracingContextProvider = TracingContextProvider(context)
+    public fun tracingContext(): TracingContext = DynamicTracingContext(context)
 
     @Bean
     public fun tracingRequestInterceptor(): RequestInterceptor =
-        com.wutsi.tracing.TracingRequestInterceptor("twitter-server", tracingContextProvider())
+        com.wutsi.tracing.TracingRequestInterceptor("twitter-server", tracingContext())
 }
